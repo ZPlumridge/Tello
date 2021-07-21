34cece38d037dc642bbec58ad7952d188604facd
Zoe Plumridge <zplumzebra@gmail.com>
2021-07-21 13:32:33 -0400

    added swift files for filter and result VCs, segue from filter VC to main VC

---
+++ Tello/FilterViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FilterViewController.swift
+//  Tello
+//
+//  Created by Zoë Plumridge on 7/21/21.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func FilterSpecializedTextBox(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Tello/filter.swift
@@ -0,0 +1,19 @@
+//
+//  filter.swift
+//  Tello
+//
+//  Created by Zoë Plumridge on 7/21/21.
+//
+
+import Foundation
+
+
+/*class filterResults {
+    var specialtyFilter : String = ""
+    
+    if specialtyInput == "anxiety" || specialtyInput == "Anxiety" || specialtyInput == "ANXIETY" {
+        specialtyFilter = "anxiety"
+    } else if specialtyInput == "adhd" or specialtyInput == "ADHD" {
+        specialtyFilter = "adhd"
+    }
+}*/
