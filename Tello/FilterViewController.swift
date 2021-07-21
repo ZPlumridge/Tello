7758caae838808291198f5fee33c56bf719b247f
Trinity Lee <trinity.l5683@gmail.com>
2021-07-21 14:22:00 -0400

    added dictionary content

--- Tello/FilterViewController.swift
+++ Tello/FilterViewController.swift
@@ -16,7 +16,8 @@ class FilterViewController: UIViewController {
     }
     
     var possibleSpecialties = ["anxiety", "adhd"]
-    var doctors = ["Lila Massoumi": ["adhd", "anxiety", "BCBS"]]
+    var doctors = ["Lila Massoumi": ["adhd", "anxiety", "BCBS"], "Hetal Patel": ["anxiety", "asr", "aetna", "beacan health options", "blue cross blue shield", "blue care network", "cigna", "cofinity", "hap", "magellan","McLaren", "Medicare", "Medicare plus Blue", "Meridian", "Molina", "Priority Health", "Tricare", "United Healthcare/OPTUM", "Humana", "Wellcare"  ], "Sarah Hutton": ["anxiety", "adhd", "HAP", "Cofinity", "Aetna", "Blue Cross Blue Shield", "Priority Health"], "Nancy Cheng": ["anxiety", "blue cross blue shield"], "Cathrine B. Frank": ["adhd", "Blue cross blue shield"], "Jimmie Leleszi": ["anxiety", "adhd", "blue cross blue shield", "pediatric"], "Brooke Weingarden": ["anxiety", "adhd", "blue cross blue shield", "pediatric"], "Randy Dean": ["anxiety", "adhd", "blue cross blue shield", "pediatric"], "Gabriella Geiszt": ["anxiety", "adhd","Aetna", "Anthem", "Anthem Blue Cross Blue Shield", "CareFirst Blue Cross Blue Shield","Blue cross blue shield", "Cigna","multiplan", "priority health", "pediatric"], "Kelly Rogalski": ["anxiety", "adhd", "pediatric"], "Erin Fallucca": ["anxiety", "adhd", "blue cross blue shield", "pediatric"]]
+
     
     @IBOutlet weak var FilterSpecialtyTextBox: UITextField!
     
